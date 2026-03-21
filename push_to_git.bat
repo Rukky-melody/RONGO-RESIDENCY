5f67d67dc66bdc7e0bc6eb61df121771e2deeb44
Rukky-melody <ofumanamelody350@gmail.com>
2026-03-21 11:26:41 -0700

    Merge About Us content into Homepage and add animations (Clean Repository)

--- push_to_git.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-git init
-git remote remove origin
-git remote add origin "https://github.com/Rukky-melody/RONGO-RESIDENCY.git"
-git add .
-git commit -m "Merge About Us content into Homepage and add animations"
-git branch -M main
-git push -u origin main
